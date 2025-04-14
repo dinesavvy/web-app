26a6dcb70f86a134304b10cb6ae7fa06c7b5c0d7
shreyash-theintellify <shreyash.z@theintellify.com>
2025-04-14 11:25:26 +0530

    remove stripe key

--- src/panels/merchant/auth/PaymentSidebar.jsx
+++ src/panels/merchant/auth/PaymentSidebar.jsx
@@ -13,9 +13,9 @@ import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "./CheckoutForm"; // Make sure you have this form to handle the payment process
 import { Modal } from "antd";
 
-const stripePromise = loadStripe(
-  "pk_test_51QhDunE3An7OFziSHkki0YdsnYqIM3tg0ZYHSvovC2F6FFphzxZu58bZII3VKVtpy8Kl43Q4kaIQ7V7UmbYz36zf00c5PLSjvH"
-);
+// const stripePromise = loadStripe(
+//   "pk_test_51QhDunE3An7OFziSHkki0YdsnYqIM3tg0ZYHSvovC2F6FFphzxZu58bZII3VKVtpy8Kl43Q4kaIQ7V7UmbYz36zf00c5PLSjvH"
+// );
 
 const PaymentSidebar = ({
   isPaymentSidebar,
@@ -39,62 +39,62 @@ const PaymentSidebar = ({
     dispatch(businessAddNudgeCreditHandler(payload));
   };
 
-  const createStripePrice = async () => {
-    try {
-      const res = await fetch("https://api.stripe.com/v1/prices", {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer sk_test_51QhDunE3An7OFziSoNkwsbbgiI4VRtOjzX4CyDdobRvuuDrkSr7I5VvF4Sd8iVrkZnRQqzBR5gYYIbTUVNti9WZZ00A2vlz00M`,
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
+  // const createStripePrice = async () => {
+  //   try {
+  //     const res = await fetch("https://api.stripe.com/v1/prices", {
+  //       method: "POST",
+  //       headers: {
+  //         Authorization: `Bearer sk_test_51QhDunE3An7OFziSoNkwsbbgiI4VRtOjzX4CyDdobRvuuDrkSr7I5VvF4Sd8iVrkZnRQqzBR5gYYIbTUVNti9WZZ00A2vlz00M`,
+  //         "Content-Type": "application/x-www-form-urlencoded",
+  //       },
 
-        body: new URLSearchParams({
-          currency: "usd",
-          unit_amount: activeNudge,
-          // "recurring[interval]": "month",
-          "product_data[name]": "Nudge credit",
-        }),
-      });
+  //       body: new URLSearchParams({
+  //         currency: "usd",
+  //         unit_amount: activeNudge,
+  //         // "recurring[interval]": "month",
+  //         "product_data[name]": "Nudge credit",
+  //       }),
+  //     });
 
-      const data = await res.json();
-      if (data?.id) {
-        // const res1 = await fetch("https://api.stripe.com/v1/payment_links", {
-        //   method: "POST",
-        //   headers: {
-        //     Authorization: `Bearer sk_test_51QhDunE3An7OFziSoNkwsbbgiI4VRtOjzX4CyDdobRvuuDrkSr7I5VvF4Sd8iVrkZnRQqzBR5gYYIbTUVNti9WZZ00A2vlz00M`,
-        //     "Content-Type": "application/x-www-form-urlencoded",
-        //   },
-        //   body: new URLSearchParams({
-        //     "line_items[0][price]": data?.id,
-        //     "line_items[0][quantity]": activeNudge,
-        //     "after_completion[type]": "redirect",
-        //     "after_completion[redirect][url]":
-        //       "http://localhost:3001/merchant/nudges",
-        //   }),
-        // });
-        const res1 = await fetch("https://api.stripe.com/v1/checkout/sessions", {
-          method: "POST",
-          // headers: {
-          //   Authorization: `Bearer sk_test_51QhDunE3An7OFziSoNkwsbbgiI4VRtOjzX4CyDdobRvuuDrkSr7I5VvF4Sd8iVrkZnRQqzBR5gYYIbTUVNti9WZZ00A2vlz00M`,
-          //   "Content-Type": "application/x-www-form-urlencoded",
-          // },
-          body: new URLSearchParams({
-            "line_items[0][price]": data?.id,
-            "line_items[0][quantity]": activeNudge,
-            mode: "payment",
-            currency: "usd", // optional here if price already defines it
-            "success_url": "http://localhost:3001/merchant/nudges?success=true",
-            "cancel_url": "http://localhost:3001/merchant/nudges?cancelled=true",
-          }),
-        });
-        const finalUrl = await res1.json();
+  //     const data = await res.json();
+  //     if (data?.id) {
+  //       // const res1 = await fetch("https://api.stripe.com/v1/payment_links", {
+  //       //   method: "POST",
+  //       //   headers: {
+  //       //     Authorization: `Bearer sk_test_51QhDunE3An7OFziSoNkwsbbgiI4VRtOjzX4CyDdobRvuuDrkSr7I5VvF4Sd8iVrkZnRQqzBR5gYYIbTUVNti9WZZ00A2vlz00M`,
+  //       //     "Content-Type": "application/x-www-form-urlencoded",
+  //       //   },
+  //       //   body: new URLSearchParams({
+  //       //     "line_items[0][price]": data?.id,
+  //       //     "line_items[0][quantity]": activeNudge,
+  //       //     "after_completion[type]": "redirect",
+  //       //     "after_completion[redirect][url]":
+  //       //       "http://localhost:3001/merchant/nudges",
+  //       //   }),
+  //       // });
+  //       const res1 = await fetch("https://api.stripe.com/v1/checkout/sessions", {
+  //         method: "POST",
+  //         // headers: {
+  //         //   Authorization: `Bearer sk_test_51QhDunE3An7OFziSoNkwsbbgiI4VRtOjzX4CyDdobRvuuDrkSr7I5VvF4Sd8iVrkZnRQqzBR5gYYIbTUVNti9WZZ00A2vlz00M`,
+  //         //   "Content-Type": "application/x-www-form-urlencoded",
+  //         // },
+  //         body: new URLSearchParams({
+  //           "line_items[0][price]": data?.id,
+  //           "line_items[0][quantity]": activeNudge,
+  //           mode: "payment",
+  //           currency: "usd", // optional here if price already defines it
+  //           "success_url": "http://localhost:3001/merchant/nudges?success=true",
+  //           "cancel_url": "http://localhost:3001/merchant/nudges?cancelled=true",
+  //         }),
+  //       });
+  //       const finalUrl = await res1.json();
 
-        window.open(finalUrl.url, "_blank");
-      }
-    } catch (err) {
-      console.error("Error creating Stripe price:", err);
-    }
-  };
+  //       window.open(finalUrl.url, "_blank");
+  //     }
+  //   } catch (err) {
+  //     console.error("Error creating Stripe price:", err);
+  //   }
+  // };
 
   useEffect(() => {
     if (businessAddNudgeCreditSelector?.data?.statusCode === 200) {
@@ -103,7 +103,7 @@ const PaymentSidebar = ({
         content: businessAddNudgeCreditSelector?.data?.message,
       });
       setClientSecret(businessAddNudgeCreditSelector?.data?.data?.clientSecret);
-      createStripePrice();
+      // createStripePrice();
       dispatch(businessAddNudgeCreditAction.businessAddNudgCreditReset());
     } else if (businessAddNudgeCreditSelector?.message) {
       messageApi.open({
